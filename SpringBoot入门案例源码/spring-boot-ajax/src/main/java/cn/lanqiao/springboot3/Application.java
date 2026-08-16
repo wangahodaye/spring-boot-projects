583340f289f323192db62d90b5f446e1c33cda99
wanghao <2296542308@qq.com>
2026-08-16 23:40:16 +0100

    修改

--- SpringBoot入门案例源码/spring-boot-ajax/src/main/java/cn/lanqiao/springboot3/Application.java
+++ SpringBoot入门案例源码/spring-boot-ajax/src/main/java/cn/lanqiao/springboot3/Application.java
@@ -9,6 +9,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("启动 Spring Boot...");
         System.out.println("Spring Boot 项目实践之 Ajax 技术使用教程");
+        System.out.println("http://127.0.0.1:8080/ajax-test.html");
+
+
         SpringApplication.run(Application.class, args);
     }
 }
